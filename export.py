fddf4c3a312fcb5fa266a97b605484eb14ec2367
tylen <tylencallander@gmail.com>
2024-06-13 14:14:18 -0400

    fixed security issues.

--- export.py
+++ export.py
@@ -1,36 +1,44 @@
 import pandas as pd
 import json
 
-# Creating the Excel file
+# Creating the Excel file from the projects.json file
 
 def create_excel_from_json(json_file, output_file):
     try:
         with open(json_file, 'r') as file:
             data = json.load(file)
     except FileNotFoundError:
-        print(f"\nError: JSON file, in path '{json_file}' not found.")
+        print(f"\nError: JSON file at '{json_file}' not found.")
         return
 
     print("\nImporting projects.json file...")
 
-# Making all of the folder paths into hyperlinks.
+    project_data = []
+    for key, val in data.items():
+        project_data.append({
+            'Project Number': key,
+            'Project Path': val['projectfullpath']
+        })
 
-    project_data = [{'Project Number': key, 'Project Path': f"=HYPERLINK(\"{val['projectfullpath']}\", \"{val['projectfullpath']}\")"} for key, val in data.items()]
     df = pd.DataFrame(project_data)
 
     print("\nCreating Excel sheet...")
 
-# Making the folder paths blue so people can actually tell its a hyperlink
+# Setting the paths as hyperlinks
 
     with pd.ExcelWriter(output_file, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Project Links')
 
-        workbook = writer.book
+        workbook  = writer.book
         worksheet = writer.sheets['Project Links']
+
+# Manually making the paths blue and underlined so the user can actually tell its a link
 
         link_format = workbook.add_format({'font_color': 'blue', 'underline': 1})
 
-        worksheet.set_column('B:B', None, link_format)
+        for idx, val in enumerate(project_data, start=2): 
+            url = f"=HYPERLINK(\"{val['Project Path']}\", \"{val['Project Path']}\")"
+            worksheet.write_url(f'B{idx}', val['Project Path'], link_format, val['Project Path'])
 
     print("\nExcel file successfully created!")
 
