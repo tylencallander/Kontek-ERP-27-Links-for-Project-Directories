5b0603d48497ca087b9208340f5019427b620075
tylen <tylencallander@gmail.com>
2024-06-13 11:25:03 -0400

    added error msg for debugging

--- export.py
+++ export.py
@@ -4,8 +4,13 @@ import pandas as pd
 # Creating Excel file & exporting projects.json file
 
 def create_excel_from_json(json_file, output_file):
-    with open(json_file, 'r') as file:
-        data = json.load(file)
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f"\nError: JSON file, in path '{json_file}' not found.")
+        return
+    
     print("\nImporting projects.json file...")
     
     project_data = []
@@ -16,12 +21,12 @@ def create_excel_from_json(json_file, output_file):
     df = pd.DataFrame(project_data, columns=['Project Number', 'Project Path'])
     
     df.to_excel(output_file, index=False)
+    print("\nExcel file created successfully.")
 
 def main():
     json_file_path = "P:/KONTEK/ENGINEERING/ELECTRICAL/Application Development/ERP/3. ConRec Folder Search/V3_2024_06_12/projects.json"
     output_excel_file = "projectlinks.xlsx"
     create_excel_from_json(json_file_path, output_excel_file)
-    print("\nExcel file created successfully.")
 
 if __name__ == "__main__":
     main()
